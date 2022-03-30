1b5b46df4645d8feeaec54a425f7a8065d9e0a39
brettmadrid <brettmadrid9@gmail.com>
2022-03-29 19:39:37 -0700

    navbar links added

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/Layout"
 
-const Recipes = () => {
+const recipes = () => {
   return (
     <Layout>
       <h1>recipes page</h1>
@@ -9,4 +9,4 @@ const Recipes = () => {
   )
 }
 
-export default Recipes
+export default recipes
--- src/pages/Tags.js
+++ src/pages/Tags.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/Layout"
 
-const Tags = () => {
+const tags = () => {
   return (
     <Layout>
       <h1>tags page</h1>
@@ -9,4 +9,4 @@ const Tags = () => {
   )
 }
 
-export default Tags
+export default tags
