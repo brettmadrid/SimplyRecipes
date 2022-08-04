1c9e2fd685ac3e9681052210a8cc23e2942b715b
brettmadrid <brettmadrid9@gmail.com>
2022-08-04 13:22:37 -0700

    set up navbar component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Navbar from "../components/Navbar"
 
-export default function Home() {
+const about = () => {
   return (
     <>
       <Navbar />
-      <h2>Hello world!</h2>
+      <h2>about</h2>
     </>
   )
 }
+
+export default about
