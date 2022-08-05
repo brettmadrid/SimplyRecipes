e0b4f5180bd2335d723b4a664b1b0efc55569394
brettmadrid <brettmadrid9@gmail.com>
2022-08-05 15:36:12 -0700

    installed gatsby-plugin-image dependency

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
+import Images from "../components/Images"
 import Layout from "../components/Layout"
 
 export default function Home() {
   return (
     <Layout>
       <h1>home</h1>
+      <Images />
     </Layout>
   )
 }
