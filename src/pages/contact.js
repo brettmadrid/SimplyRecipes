90438bc0d14c52d5ddd2a0bc79d7e550e024d7ba
brettmadrid <brettmadrid9@gmail.com>
2022-08-04 22:36:53 -0700

    completed contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,7 +5,48 @@ import styled from "styled-components"
 const contact = () => {
   return (
     <Layout>
-      <h1>contact page</h1>
+      <main className="page">
+        <section className="contact-page">
+          <article className="contact-info">
+            <h3>Want to get in touch?</h3>
+            <p>
+              I'm baby yuccie retro semiotics shoreditch gluten-free, bitters
+              glossier. Semiotics fit church-key coloring book, kombucha
+              kickstarter migas +1 trust fund actually kale chips vegan tacos.
+            </p>
+            <p>
+              Yes plz lo-fi kickstarter art party knausgaard mukbang neutra 3
+              wolf moon readymade squid food truck single-origin coffee
+              affogato.
+            </p>
+            <p>
+              PBR&B yr iPhone, kickstarter waistcoat narwhal direct trade vape
+              activated charcoal health goth irony hoodie. XOXO affogato cronut,
+              hell of mumblecore vexillologist venmo gentrify paleo helvetica
+              90's.
+            </p>
+          </article>
+          <article>
+            <form action="" className="form contact-form">
+              <div className="form-row">
+                <label htmlFor="name">your name</label>
+                <input type="text" name="name" id="name" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="email">your email</label>
+                <input type="text" name="email" id="email" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="message">message</label>
+                <textarea name="message" id="message"></textarea>
+              </div>
+              <button type="submit" className="btn block">
+                submit
+              </button>
+            </form>
+          </article>
+        </section>
+      </main>
     </Layout>
   )
 }
