640949e83d64e6620070d56f8f607f0976c57f1f
brettmadrid <brettmadrid9@gmail.com>
2022-08-12 17:17:29 -0700

    queried data from contentful into AllRecipes

--- src/components/AllRecipes.js
+++ src/components/AllRecipes.js
@@ -1,13 +1,38 @@
 import React from "react"
 import RecipesList from "./RecipesList"
 import TagsList from "./TagsList"
+import { graphql, useStaticQuery } from "gatsby"
+
+const query = graphql`
+  query {
+    allContentfulRecipe(sort: { fields: title, order: ASC }) {
+      nodes {
+        id
+        title
+        cookTime
+        prepTime
+        content {
+          tags
+        }
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+        }
+      }
+    }
+  }
+`
 
 const AllRecipes = () => {
+  // const data = useStaticQuery(query)
+  // const recipeArray = data.allContentfulRecipe.nodes
+  const {
+    allContentfulRecipe: { nodes: recipeArray },
+  } = useStaticQuery(query)
   return (
     <div>
       <h4>all recipes</h4>
-      <TagsList />
-      <RecipesList />
+      <TagsList recipes={recipeArray} />
+      <RecipesList recipes={recipeArray} />
     </div>
   )
 }
