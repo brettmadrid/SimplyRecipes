a5b640cae0e43219e73a38733202beabbc52f178
brettmadrid <brettmadrid9@gmail.com>
2022-08-15 20:47:57 -0700

    completed about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,24 +1,29 @@
 import React from "react"
 import Layout from "../components/Layout"
 import { StaticImage } from "gatsby-plugin-image"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
+import RecipesList from "../components/RecipesList"
+// import SEO from "../components/SEO"
 
-const About = () => {
+const About = ({
+  data: {
+    allContentfulRecipe: { nodes: recipes },
+  },
+}) => {
   return (
     <Layout>
+      {/* <SEO title="About" /> */}
       <main className="page">
         <section className="about-page">
           <article>
-            <h2>The tastiest recipes site on the internet!</h2>
+            <h2>I'm baby coloring book poke taxidermy</h2>
             <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea fuga
-              architecto praesentium iusto rerum veritatis molestias unde itaque
-              repellendus, aliquam accusantium laudantium quaerat fugit nulla,
-              tempora harum dolor quos nobis!
+              Taxidermy forage glossier letterpress heirloom before they sold
+              out you probably haven't heard of them banh mi biodiesel chia.
             </p>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-              nostrum dicta nobis ea, distinctio hic doloribus.
+              Taiyaki tumblr flexitarian jean shorts brunch, aesthetic salvia
+              retro.
             </p>
             <Link to="/contact" className="btn">
               contact
@@ -26,14 +31,37 @@ const About = () => {
           </article>
           <StaticImage
             src="../assets/images/about.jpeg"
-            alt="person pouring salt in a bowl"
+            alt="Person Pouring Salt in Bowl"
             className="about-img"
-            placeholder="tracedSVG"
+            placeholder="blurred"
           />
+        </section>
+        <section className="featured-recipes">
+          <h5>Look at this Awesomesouce!</h5>
+          <RecipesList recipes={recipes} />
         </section>
       </main>
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    allContentfulRecipe(
+      sort: { fields: title, order: ASC }
+      filter: { featured: { eq: true } }
+    ) {
+      nodes {
+        id
+        title
+        cookTime
+        prepTime
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+        }
+      }
+    }
+  }
+`
 
 export default About
